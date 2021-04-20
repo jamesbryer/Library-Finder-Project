14d0d10142e259c9fb33365249ad6c0bf9f6c0b2
Sofía Rains <sofia-sudo@users.noreply.github.com>
2021-04-20 16:39:37 +0100

    Postcode filter added to table
    Table now only shows data from the user's Postcode, unless otherwise specified. If "View in Table" is selected with no options, all libraries are shown.
    Added indicator to Table page to show which postcode (if any) is being displayed.

--- Server/main.js
+++ Server/main.js
@@ -1,11 +1,31 @@
 // Access from browser with http://localhost:8080/
+const { query } = require('express');
 const express = require('express');
 const mysql = require('mysql');
 const conf = require('./conf.json');
 
-const QUERY = "SELECT * FROM `libraries`";
+let postcodeRegion = ""; // Stores the postcode region the user has entered on the homepage for table.ejs and map.ejs
+let QUERY = `SELECT * FROM isd.libraries WHERE Postcode LIKE '%${postcodeRegion}%'`; // Query for the db, MUST be updated after postcodeRegion changes.
 
 var app = express();
+
+// Dictionary mapping the value of Option to the postcode (1 : BS1, 13 : BS16 etc)
+let postcodeDictionary = {
+    "1" : "BS1",
+    "2": "BS2",
+    "3": "BS3",
+    "4": "BS4",
+    "5": "BS5",
+    "6": "BS7",
+    "7": "BS8",
+    "8": "BS9",
+    "9": "BS10",
+    "10": "BS11",
+    "11": "BS13",
+    "12": "BS14",
+    "13": "BS16",
+    "14": ""
+}; 
 
 // Configure Express to use embedded JavaScript (EJS)
 app.set("view engine", "ejs");
@@ -27,9 +47,20 @@ app.get("/map.html", function(request, response){
 });
 
 app.get("/table.html", function(request, response){
+    postcodeRegion = postcodeDictionary[request.query.option]; // Updates postcode region using the dictionary
+    console.log(postcodeRegion);
+    if (postcodeRegion != null) 
+    {
+        QUERY = `SELECT * FROM isd.libraries WHERE Postcode LIKE '%${postcodeRegion}%'`; // Updates query before use
+    }
+    else
+    {
+        QUERY = `SELECT * FROM isd.libraries`
+    }
+    
     connection.query(QUERY, function(err, rows, fields) {
         if (err) throw err;
-        response.render("table", {'rows' : rows});
+        response.render("table", {'rows' : rows, postcodeRegion});
     });
 });
 
