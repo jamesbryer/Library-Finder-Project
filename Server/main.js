16ea8e81c0260df80dab18f9b0992f295b1de400
anourthebrit <anour.taylor@gmail.com>
2021-04-28 09:34:59 +0100

    updated

--- Server/main.js
+++ Server/main.js
@@ -2,7 +2,6 @@
 const express = require('express');
 const mysql = require('mysql');
 const conf = require('./conf.json');
-process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 
 const QUERY = "SELECT * FROM `libraries` WHERE Postcode LIKE ? order by `Library Name`";
 
@@ -23,48 +22,48 @@ function splash(request, response) {
     else {
         option = request.query.option;
     }
-    response.render("index", { "option": option });
+    response.render("index", {"option":option });
 }
 
 // Splash page (index.html) is served by default
 app.get("/", splash);
 app.get("/index.html", splash);
 
-app.get("/map.html", function (request, response) {
-    connection.query(QUERY, [request.query.option + " %"], function (err, rows) {
+app.get("/map.html", function(request, response) {
+    connection.query(QUERY, [request.query.option+" %"], function(err, rows) {
         if (err) {
             response.status(500);
             response.send(err);
         }
         else {
-            response.render("map", { 'rows': rows, "option": request.query.option });
+            response.render("map", {'rows':rows, "option":request.query.option });
         }
     });
 });
 
-app.get("/table.html", function (request, response) {
-    connection.query(QUERY, [request.query.option + " %"], function (err, rows) {
+app.get("/table.html", function(request, response) {
+    connection.query(QUERY, [request.query.option+" %"], function(err, rows) {
         if (err) {
             response.status(500);
             response.send(err);
         }
         else {
-            response.render("table", { 'rows': rows, "option": request.query.option });
+            response.render("table", {'rows':rows, "option":request.query.option});
         }
     });
 });
 
 // Create connection
-var connection = mysql.createConnection(conf[process.env.NODE_ENV].db);
+var connection = mysql.createConnection(conf.db);
 
 // Use connection to connect to database
-connection.connect(function (err) {
-    if (err) {
-        console.error("Connection error: ", err.message);
-    } else {
-        console.log("Connected as: ", connection.threadId);
-    }
+connection.connect(function(err){
+	if (err) {
+    	console.error("Connection error: ", err.message);    
+	} else {
+    	console.log("Connected as: ", connection.threadId);    
+	}
 });
 
-app.listen(conf[process.env.NODE_ENV].port);
-console.log("Listening on port %s", conf[process.env.NODE_ENV].port);
+app.listen(conf.port);
+console.log("Listening on port %s", conf.port);
